572c8c8cd2a18e7cf6120922fcb0985f54a1629f
Sodoo22 <sodoo75@gmail.com>
2023-01-02 12:40:22 +0800

    up

--- day33/my-react-app-module/src/App.js
+++ day33/my-react-app-module/src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Popular Products</h2><hr></hr>
+
+
+      <div className="contaner">
+        <div>
+          <img src=" ./img/image-yellow.png" className="App-logo" alt="logo" />
+        </div>
+        <div className="textbox">
+          <h2>55</h2>
+          <p className="bluetext">Haught or Naught</p>
+          <p>High-minded or absent-minded? You decide</p>
+          <div className="contaner profile">
+            <p>Submitted by:  </p>
+            <img src=" https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" className="App-logo" alt="logo" />
+          </div>
+        </div>
+      </div>
+
+      <div className="contaner">
+        <div>
+          <img src=" ./img/image-aqua.png" className="App-logo" alt="logo" />
+        </div>
+        <div className="textbox">
+          <h2>44</h2>
+          <p className="bluetext">Yellow Pail</p>
+          <p>On-demand sand castle contruction expertice</p>
+          <div className="contaner profile">
+            <p>Submitted by:  </p>
+            <img src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" className="App-logo" alt="logo" />
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
