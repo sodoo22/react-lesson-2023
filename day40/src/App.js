61fa0cd56ec88bf8119d48212983a13841585e24
Sodoo22 <sodoo75@gmail.com>
2023-01-11 13:37:23 +0800

    class

--- day40/src/App.js
+++ day40/src/App.js
@@ -2,7 +2,49 @@ import logo from "./logo.svg";
 import "./App.css";
 import User from "./components/User";
 
-function App() {
+async function App() {
+  // --------------------------- Promise Exercise ------------------------
+
+  const myPromise = new Promise((resolve, rejected) => {
+    let isPlaying = true;
+    if (isPlaying) {
+      return resolve(isPlaying);
+    } else {
+      return rejected(isPlaying);
+    }
+  });
+  // console.log(myPromise);
+
+  const id = 45;
+  const URL = `https://api.jikan.moe/v4/anime/${id}`;
+
+  // const fetchResult = fetch(URL);
+  // fetchResult
+  //   .then((result) => result.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+
+  const fetchedResult = await fetch(URL);
+  console.log(fetchedResult);
+  const JSONResult = await fetchedResult.json();
+  console.log(JSONResult);
+
+  // --------------------------- Promise Exercise ------------------------
+
+  // --------------------------- Callback Function Exercise ------------------------
+  function khuslen(callbackFunction) {
+    let a = 4;
+    callbackFunction(a);
+  }
+
+  function forex(param) {
+    console.log(param + 4);
+  }
+
+  // khuslen(forex);
+  // --------------------------- Callback Function Exercise------------------------
+
   return (
     <div className="App">
       <h1>Day-40 React States</h1>
