d9ff2b11865623bad8d9c42f040c05c069dc8279
Sodoo22 <sodoo75@gmail.com>
2023-01-31 12:46:52 +0800

    cl

--- admin-panel-project/fe/src/App.js
+++ admin-panel-project/fe/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Users from "./pages/Users";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Admin Panel Project</h1>
+      {/* <h1>Admin Panel Project</h1> */}
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
       </Routes>
     </div>
