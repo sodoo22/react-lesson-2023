300365bd98c2b2c2f5e5da70daa1ac3a60048c83
Sodoo22 <sodoo75@gmail.com>
2023-01-31 11:40:07 +0800

    admin folders

--- admin-panel-project/be/app.js
+++ admin-panel-project/be/app.js
@@ -1,1 +1,16 @@
 console.log("it is my app");
+// import necessary modules
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+// configuration of modules
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
