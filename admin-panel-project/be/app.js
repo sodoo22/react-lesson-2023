1395c06bbd45aa9de4fb468070fec6f424c7dabb
sodoo22 <sodoo75@gmail.com>
2023-02-01 11:00:23 +0800

    cl

--- admin-panel-project/be/app.js
+++ admin-panel-project/be/app.js
@@ -3,6 +3,8 @@ console.log("it is my app");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const { request } = require("http");
+const { response } = require("express");
 
 // configuration of modules
 const app = express();
@@ -10,6 +12,23 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/users", (request, response) => {
+  fs.readFile("./data/users.json", "utf-8", (readError, readData) => {
+    if (readError) {
+      response.json({
+        status: "file reader error",
+        data: [],
+      });
+    }
+    const ObjectData = JSON.parse(readData);
+
+    response.json({
+      status: "success",
+      data: ObjectData,
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- admin-panel-project/fe/src/components/UsersTable.jsx
+++ admin-panel-project/fe/src/components/UsersTable.jsx
@@ -1,10 +1,34 @@
+import { Button } from "@mui/material";
+import { Box, Stack } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 
 export default function UsersTable() {
+  const URL = "http://localhost:8080/users";
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  async function fetchAllData() {
+    const FETCHED_DATA = await fetch(URL); // Response
+    const FETCHED_JSON = await FETCHED_DATA.json(); // {status: 'success, data: [{id: ...}]}
+    console.log(FETCHED_JSON);
+    setUsers(FETCHED_JSON.data);
+  }
+  console.log(users);
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
     { field: "firstName", headerName: "First name", width: 130 },
     { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "email", headerName: "Email", width: 130 },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      type: "number",
+      width: 110,
+    },
     {
       field: "age",
       headerName: "Age",
@@ -20,6 +44,25 @@ export default function UsersTable() {
       valueGetter: (params) =>
         `${params.row.firstName || ""} ${params.row.lastName || ""}`,
     },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 160,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box width="100%">
+            <Stack direction="row" spacing={2}>
+              <Button variant="outlined" color="success">
+                Edit
+              </Button>
+              <Button variant="outlined" color="error">
+                Delete
+              </Button>
+            </Stack>
+          </Box>
+        );
+      },
+    },
   ];
 
   const rows = [
@@ -33,15 +76,18 @@ export default function UsersTable() {
     { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
     { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
+
   return (
     <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
+      {users && (
+        <DataGrid
+          rows={users}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      )}
     </div>
   );
 }
