e8f1e7c36c36d1476b0b89ad8654f3dab19b98f8
Sodoo22 <sodoo75@gmail.com>
2023-03-16 12:43:35 +0800

    up

--- day77-mongoose-setup/be/routes/api.js
+++ day77-mongoose-setup/be/routes/api.js
@@ -8,6 +8,32 @@ Router.get("/users", async (request, response) => {
   response.json({ data: result });
 });
 
+Router.get("/user", async (request, response) => {
+  const userId = request.query.id;
+  console.log(userId);
+
+  const user = await User.findOne({ _id: userId });
+  response.json({ data: user });
+});
+
+Router.get("/userbyemail", async (request, response) => {
+  const userEmail = request.query.email;
+  console.log(userEmail);
+
+  const foundUser = await User.find({ email: userEmail }, "_id name email", {
+    sort: lastLogin,
+  }).exec();
+
+  response.json({ data: foundUser });
+});
+// Custom mehtod
+Router.get("/userGetEmail", async (reguest, response) => {
+  const userEmail = reguest.query.email;
+  const foundUser = await User.findByUserEmail(userEmail);
+
+  response.json({ data: foundUser });
+});
+
 Router.post("/user", async (request, response) => {
   const body = request.body;
   const newUser = new User(body);
@@ -27,6 +53,15 @@ Router.delete("/user", async (request, response) => {
   response.json({
     data: deletedUser,
   });
+});
+
+Router.put("/updateUser", async (request, response) => {
+  const result = await User.updateOne(
+    { email: "sodoo777999@gmail.com" },
+    { $set: { lastLogin: Date.now() } }
+  );
+
+  response.json({ data: result });
 });
 
 module.exports = Router;
