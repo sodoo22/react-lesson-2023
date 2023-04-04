7607cf4ded67cd0da13ca2dcfcdbaa5146e11aea
Sodoo22 <sodoo75@gmail.com>
2023-04-04 11:47:18 +0800

    day91

---
+++ day91-nextjs-typescript/be/index.ts
@@ -0,0 +1,37 @@
+
+import TheaterRouter from './routes/theaters.api'
+import express, {Express, Request, Response} from "express"
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from "cors"
+dotenv.config()
+
+
+
+const app: Express = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/test';
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+let name: string = "<h1>DAY-91  Next JS Typescript</h1>";
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(name);
+});
+
+app.use('/theaters', TheaterRouter)
+
+app.listen(PORT, () => {
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log("Database connected succesfully"))
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log(`Application running on http://localhost:${PORT}`);
+});
---
+++ day91-nextjs-typescript/movie-review-fe/src/pages/about.tsx
@@ -0,0 +1,5 @@
+export default function About(): JSX.Element  {
+    return(
+        <h1>I’m an about page</h1>
+    )
+}
