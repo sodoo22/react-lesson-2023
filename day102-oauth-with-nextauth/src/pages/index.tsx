33de4ee6102e8dc57dde2e1f293cea2d72b2b201
Sodoo22 <sodoo75@gmail.com>
2023-04-26 12:13:38 +0800

    class

---
+++ day102-oauth-with-nextauth/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <h1>Next.js Exaple</h1>
+      <p>Here is my nextjs auth app</p>
+    </Layout>
+  );
+}
