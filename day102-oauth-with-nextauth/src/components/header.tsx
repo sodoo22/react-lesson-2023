beeb4be82aa90a615d0686e949306d9f0b7c4ffe
Sodoo22 <sodoo75@gmail.com>
2023-04-27 12:07:24 +0800

    class

--- day102-oauth-with-nextauth/src/components/header.tsx
+++ day102-oauth-with-nextauth/src/components/header.tsx
@@ -1,21 +1,58 @@
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "../styles/header.module.css";
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   const handleLogin = (e: any) => {
     e.preventDefault();
     signIn();
   };
+  const handleLogout = (e: any) => {
+    e.preventDefautl();
+    signOut();
+  };
   return (
     <header>
-      <div>
-        <div>
+      <noscript>
+        <style>{`.nojs-show {opacity : 1; top: 0; }`}</style>
+      </noscript>
+      <div className={styles.signedInStatus}>
+        <div
+          className={`.nojs-show ${
+            !session && loading ? styles.loading : styles.loaded
+          }`}
+        >
           {!session && (
             <div>
               <span>You are not signed in</span>
               <a href={`/api/auth/signin`} onClick={handleLogin}>
                 Sign In
+              </a>
+            </div>
+          )}
+
+          {session?.user && (
+            <div>
+              {session.user.image && (
+                <span
+                  style={{ backgroundImage: `url(${session.user.image})` }}
+                  className={styles.avatar}
+                />
+              )}
+              <span className={styles.signedInText}>
+                <small>Signed in as </small>{" "}
+                <strong>{session.user.name || session.user.name}</strong>
+                <br />
+                <strong>{session.user.email || session.user.name}</strong>
+              </span>
+              <a
+                href={`/api/auth/signout`}
+                className={styles.button}
+                onClick={handleLogout}
+              >
+                Sign out
               </a>
             </div>
           )}
